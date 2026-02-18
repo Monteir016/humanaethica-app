9cb2ebd8569eaa363fd61c2b57f314cd5d3ac8ee
Antonio Rito Silva <rito.silva@tecnico.ulisboa.pt>
2026-02-18 08:30:36 +0000

    feat(shifts): Implement and test web service getShiftsByActivity
    Closes #26

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/shift/ShiftController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/shift/ShiftController.java
@@ -6,16 +6,23 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.shift.dto.ShiftDto;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/activities/{activityId}/shift")
+@RequestMapping("/activities/{activityId}")
 public class ShiftController {
 
     @Autowired
     private ShiftService shiftService;
 
-    @PostMapping
+    @PostMapping("/shift")
     @PreAuthorize("hasRole('ROLE_MEMBER') and hasPermission(#activityId, 'ACTIVITY.MEMBER')")
     public ShiftDto createShift(@PathVariable Integer activityId, @Valid @RequestBody ShiftDto shiftDto) {
         return shiftService.createShift(activityId, shiftDto);
+    }
+
+    @GetMapping("/shifts")
+    public List<ShiftDto> getShiftsByActivity(@PathVariable Integer activityId) {
+        return shiftService.getShiftsByActivity(activityId);
     }
 }
